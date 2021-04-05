3744f54a3443ac1c988519c61e1703d28c11b957
Mohamed Saad <mohamedsaadamin.cis@gmail.com>
2021-04-05 23:15:39 +0200

    Improve collision performance with new object channels

--- Source/HashDash/HashDashCharacter.cpp
+++ Source/HashDash/HashDashCharacter.cpp
@@ -124,6 +124,12 @@ void AHashDashCharacter::EndAttack()
 
 void AHashDashCharacter::OnWeaponBeginOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
 {
+	AHashDashCharacter* temp = Cast<AHashDashCharacter>(OtherActor);
+	if (temp)
+	{
+		GEngine->AddOnScreenDebugMessage(-1, 5.f, FColor::Yellow, FString::Printf(TEXT("%s"), *temp->GetName()));
+		return;
+	}
 	AEnemyCharacter* Enemy = Cast<AEnemyCharacter>(OtherActor);
 	if (Enemy)
 	{
